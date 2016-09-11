6828e1709c77c25348c9fd196359f07ea2b81b55
Pavel Sorokin <pavel.sorokin@nerro.eu>
2016-09-11 23:21:37 +0200

    Add 'usage' description

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	printUsage()
+}
+
+func printUsage() {
+	fmt.Println("Usage: fixla [OPTION]")
+	fmt.Println("A bundle of useful utilities for files handling")
+	fmt.Println("")
+	return
+}
