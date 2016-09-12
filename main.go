e0b114ab05f8c71394289accf64da0ba6c047792
Pavel Sorokin <pavel.sorokin@nerro.eu>
2016-09-13 00:30:31 +0200

    Implement '--help' and '--version' options

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main() {
-	printUsage()
+func init() {
+	if len(os.Args) == 1 {
+		printHelp()
+		os.Exit(0)
+	}
+	// check for '--version' and '--help' options
+	for _, arg := range os.Args {
+		if arg == "--version" || arg == "-v" {
+			fmt.Println(FormattedVersion())
+			os.Exit(0)
+		}
+		if arg == "--help" || arg == "-h" {
+			printHelp()
+			os.Exit(0)
+		}
+	}
 }
 
-func printUsage() {
-	fmt.Println("Usage: fixla [OPTION]")
-	fmt.Println("A bundle of useful utilities for files handling")
-	fmt.Println("")
-	return
+func main() {
+	fmt.Println("to be implemented...")
+}
+
+// printHelp provides a pretty usage message including a list of the correct
+// commands, arguments and options acceptable to the program.
+func printHelp() {
+	fmt.Println("usage: fixla [--version] [--help] <command> [<args>]")
+	fmt.Println()
+	fmt.Println("Available commands are:")
+	fmt.Println("   trailing-whitespaces   Handle trailing whitespaces")
+	fmt.Println()
 }
